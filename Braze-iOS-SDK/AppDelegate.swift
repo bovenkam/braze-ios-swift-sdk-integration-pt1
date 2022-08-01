dde3d977cb4939ae9c82f1fc9dfbf668ab2efa73
Martijn van de Bovenkamp <45668793+bovenkam@users.noreply.github.com>
2022-08-01 17:07:26 +0100

    Initial Commit

---
+++ Braze-iOS-SDK/AppDelegate.swift
@@ -0,0 +1,34 @@
+//
+//  AppDelegate.swift
+//  Braze-iOS-SDK
+//
+//  Created by Martijn van de Bovenkamp on 5/9/22.
+//  Copyright © 2022 Martijn van de Bovenkamp. All rights reserved.
+//
+
+import UIKit
+import BrazeKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    static var braze: Braze? = nil
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        // Insert Braze instance configuration here
+        
+        
+        // Insert custom configuration here
+        
+    
+        // Insert logging information here
+        
+    
+        return true
+    }
+
+}
+
---
+++ Braze-iOS-SDK/BrazeManager.swift
@@ -0,0 +1,12 @@
+//
+//  BrazeManager.swift
+//  Braze-iOS-SDK
+//
+//  Created by Martijn van de Bovenkamp on 01/08/2022.
+//  Copyright © 2022 Ian Abels. All rights reserved.
+//
+
+import Foundation
+import BrazeKit
+
+static var braze: Braze? = nil
